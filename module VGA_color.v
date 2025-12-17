c30270265c70b7cd559385521c67f9d67f7ab639
2063F <kuhae233.3001@gmail.com>
2025-12-17 14:53:10 +0900

    color　横棒表示

--- module VGA_color.v
+++ module VGA_color.v
@@ -6,3 +6,21 @@ module VGA_color(
     output reg [1:0] g,
     output reg [1:0] b
 );
+always @(posedge clk) begin
+    case(vcount / 50)
+        0: r = 0;
+        1: r = 1;
+        2: r = 2;
+        3: r = 3;
+        4: g = 0;
+        5: g = 1;
+        6: g = 2;
+        7: g = 3;
+        8: b = 0;
+        9: b = 1;
+        10: b = 2;
+        11: b = 3;
+        default: r = 0; g = 0; b = 0;
+    endcase
+end
+endmodule
